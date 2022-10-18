9c27dc0d08bc9247a730e8c8f8f28b37337f9c60
andre.coelho <coelho@hotmart.com>
2022-10-18 17:11:33 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from elasticsearch import Elasticsearch
+from pathlib import Path
+from eland.ml.pytorch import PyTorchModel
+from eland.ml.pytorch.transformers import TransformerModel
+
+
+def get_client_es():
+    return Elasticsearch(
+        hosts=[{'scheme': 'http', 'host': 'localhost', 'port': 9200}],
+        request_timeout=300,
+        verify_certs=False
+    )
+
+
+if __name__ == '__main__':
+    tm = TransformerModel("elastic/distilbert-base-cased-finetuned-conll03-english", "ner")
+    tmp_path = "models"
+    Path(tmp_path).mkdir(parents=True, exist_ok=True)
+    model_path, config, vocab_path = tm.save(tmp_path)
+    #es = elasticsearch.Elasticsearch("http://localhost:9200", timeout=300, verify_certs=False)  # 5 minute timeout
+    ptm = PyTorchModel(get_client_es(), tm.elasticsearch_model_id())
+    ptm.import_model(model_path=model_path, config_path=None, vocab_path=vocab_path, config=config)
